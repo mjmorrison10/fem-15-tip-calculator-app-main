17f8427c23af1b5bad1c1b71747b24cecfd7624b
mjmorrison10 <86391005+mjmorrison10@users.noreply.github.com>
2021-11-14 22:28:22 -0800

    added error message

--- script.js
+++ script.js
@@ -1,51 +1,65 @@
 const Buttons = document.querySelectorAll(".percent-button");
 const BillNumber = document.querySelector(".bill-number");
 const percentCustom = document.querySelector(".custom");
+const BillError = document.querySelector(".bill-error");
 
 Buttons.forEach((button) => {
   button.addEventListener("click", function () {
-    var percent = button.innerHTML.substring(0, button.innerHTML.length - 1);
+    if (BillNumber.value == "") {
+      // alert("Please enter a bill number");
+      BillError.style.display = "block";
+    } else {
+      BillError.style.display = "none";
+      var percent = button.innerHTML.substring(0, button.innerHTML.length - 1);
 
-    var bill = BillNumber.value;
-    var tip = (bill * percent) / 100;
+      var bill = BillNumber.value;
+      var tip = (bill * percent) / 100;
 
-    var numberOfPeople = document.querySelector(".numberofpeople").value;
-    var total = parseInt(bill) + parseInt(tip);
-    var tipDisplay = document.querySelector(".tip-amount-cash");
-    var totalDisplay = document.querySelector(".tip-total-cash");
+      var numberOfPeople = document.querySelector(".numberofpeople").value;
+      var total = parseInt(bill) + parseInt(tip);
 
-    if (numberOfPeople !== "") {
-      console.log(numberOfPeople);
-      tip = tip / numberOfPeople;
-      total = total / numberOfPeople;
+      var tipDisplay = document.querySelector(".tip-amount-cash");
+      var totalDisplay = document.querySelector(".tip-total-cash");
+
+      if (numberOfPeople !== "") {
+        console.log(numberOfPeople);
+        tip = tip / numberOfPeople;
+        total = total / numberOfPeople;
+      }
+
+      tipDisplay.innerHTML = tip.toFixed(2);
+      totalDisplay.innerHTML = total;
     }
-
-    tipDisplay.innerHTML = tip.toFixed(2);
-    totalDisplay.innerHTML = total;
   });
 });
 
 percentCustom.addEventListener("keyup", function (e) {
   if (e.keyCode === 13) {
-    console.log(percentCustom.value);
+    if (BillNumber.value == "") {
+      BillError.style.display = "block";
+    } else {
+      BillError.style.display = "none";
 
-    var percent = percentCustom.value;
+      console.log(percentCustom.value);
 
-    var bill = BillNumber.value;
-    var tip = (bill * percent) / 100;
+      var percent = percentCustom.value;
 
-    var numberOfPeople = document.querySelector(".numberofpeople").value;
-    var total = parseInt(bill) + parseInt(tip);
-    var tipDisplay = document.querySelector(".tip-amount-cash");
-    var totalDisplay = document.querySelector(".tip-total-cash");
+      var bill = BillNumber.value;
+      var tip = (bill * percent) / 100;
 
-    if (numberOfPeople !== "") {
-      console.log(numberOfPeople);
-      tip = tip / numberOfPeople;
-      total = total / numberOfPeople;
+      var numberOfPeople = document.querySelector(".numberofpeople").value;
+      var total = parseInt(bill) + parseInt(tip);
+      var tipDisplay = document.querySelector(".tip-amount-cash");
+      var totalDisplay = document.querySelector(".tip-total-cash");
+
+      if (numberOfPeople !== "") {
+        console.log(numberOfPeople);
+        tip = tip / numberOfPeople;
+        total = total / numberOfPeople;
+      }
+
+      tipDisplay.innerHTML = tip.toFixed(2);
+      totalDisplay.innerHTML = total;
     }
-
-    tipDisplay.innerHTML = tip.toFixed(2);
-    totalDisplay.innerHTML = total;
   }
 });
