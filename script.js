49055d71f3a3996e2eab448e31ec49a127ed96ad
mjmorrison10 <86391005+mjmorrison10@users.noreply.github.com>
2021-11-14 22:35:29 -0800

    error message

--- script.js
+++ script.js
@@ -1,34 +1,32 @@
-const Buttons = document.querySelectorAll(".percent-button");
-const BillNumber = document.querySelector(".bill-number");
-const percentCustom = document.querySelector(".custom");
-const BillError = document.querySelector(".bill-error");
+const Buttons = document.querySelectorAll(".percent-button"); // Get all the buttons
+const BillNumber = document.querySelector(".bill-number"); // bill number input
+const percentCustom = document.querySelector(".custom"); //custom percent
+const BillError = document.querySelector(".bill-error"); // Error message for bill number
 
 Buttons.forEach((button) => {
   button.addEventListener("click", function () {
     if (BillNumber.value == "") {
-      // alert("Please enter a bill number");
-      BillError.style.display = "block";
+      BillError.style.display = "block"; // show error message
     } else {
-      BillError.style.display = "none";
-      var percent = button.innerHTML.substring(0, button.innerHTML.length - 1);
+      BillError.style.display = "none"; // hide error message
+      var percent = button.innerHTML.substring(0, button.innerHTML.length - 1); // get percent from button
 
-      var bill = BillNumber.value;
-      var tip = (bill * percent) / 100;
+      var bill = BillNumber.value; // get bill value
+      var tip = (bill * percent) / 100; // calculate tip
 
-      var numberOfPeople = document.querySelector(".numberofpeople").value;
-      var total = parseInt(bill) + parseInt(tip);
+      var numberOfPeople = document.querySelector(".numberofpeople").value; // get number of people
+      var total = parseInt(bill) + parseInt(tip); // calculate total
 
-      var tipDisplay = document.querySelector(".tip-amount-cash");
-      var totalDisplay = document.querySelector(".tip-total-cash");
+      var tipDisplay = document.querySelector(".tip-amount-cash"); // get tip display
+      var totalDisplay = document.querySelector(".tip-total-cash"); // get total display
 
       if (numberOfPeople !== "") {
-        console.log(numberOfPeople);
-        tip = tip / numberOfPeople;
-        total = total / numberOfPeople;
+        tip = tip / numberOfPeople; // calculate tip per person
+        total = total / numberOfPeople; // calculate total per person
       }
 
-      tipDisplay.innerHTML = tip.toFixed(2);
-      totalDisplay.innerHTML = total;
+      tipDisplay.innerHTML = tip.toFixed(2); // display tip
+      totalDisplay.innerHTML = total; // display total
     }
   });
 });
